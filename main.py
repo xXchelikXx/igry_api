be741c8c4df70599bbbed9df5630def2d9e0d547
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2025-11-23 14:55:00 +0300

    Поправил код

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
 def get_screenshots(api_key, best_game):
     screenshots_url = f"https://api.rawg.io/api/games/{best_game['slug']}/screenshots"
-    params = {"key": api_key}
+    params = {
+        "key": api_key
+    }
     response = requests.get(screenshots_url, params=params)
     response.raise_for_status()
     screenshots = response.json()["results"]
@@ -13,7 +16,9 @@ def get_screenshots(api_key, best_game):
 
 def get_stores(api_key, best_game):
     stores_url = f"https://api.rawg.io/api/games/{best_game['slug']}/stores"
-    params = {"key": api_key}
+    params = {
+        "key": api_key
+    }
     response = requests.get(stores_url, params=params)
     response.raise_for_status()
     stores = response.json()["results"]
@@ -22,7 +27,16 @@ def get_stores(api_key, best_game):
 
 def get_best_games(api_key):
     games_url = f"https://api.rawg.io/api/games"
-    params = {"key": api_key, "page": 1, "page_size": 10, "genres": "strategy", "metacritic": "70,100", "tags": "multiplayer"}
+    pages_amount = 1
+    page_size = 10
+    params = {
+        "key": api_key,
+        "page": pages_amount,
+        "page_size": page_size,
+        "genres": "strategy",
+        "metacritic": "70,100",
+        "tags": "multiplayer"
+    }
     response = requests.get(games_url, params=params)
     response.raise_for_status()
     response_results = response.json()["results"]
