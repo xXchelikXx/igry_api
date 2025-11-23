f9d0dd9a708fbe306e9467144dbb233f95db1bc7
xXchelikXx <162018732+xXchelikXx@users.noreply.github.com>
2025-11-23 14:43:01 +0300

    Улучшил код

--- main.py
+++ main.py
@@ -2,18 +2,29 @@ import requests
 import os
 from dotenv import load_dotenv
 
+def get_screenshots(api_key, best_game):
+    screenshots_url = f"https://api.rawg.io/api/games/{best_game['slug']}/screenshots"
+    params = {"key": api_key}
+    response = requests.get(screenshots_url, params=params)
+    response.raise_for_status()
+    screenshots = response.json()["results"]
+    return screenshots
 
-load_dotenv()
-api_key = os.getenv("api_key")
+
+def get_stores(api_key, best_game):
+    stores_url = f"https://api.rawg.io/api/games/{best_game['slug']}/stores"
+    params = {"key": api_key}
+    response = requests.get(stores_url, params=params)
+    response.raise_for_status()
+    stores = response.json()["results"]
+    return stores
 
 
-def get_best_games():
+def get_best_games(api_key):
     games_url = f"https://api.rawg.io/api/games"
-
     params = {"key": api_key, "page": 1, "page_size": 10, "genres": "strategy", "metacritic": "70,100", "tags": "multiplayer"}
     response = requests.get(games_url, params=params)
     response.raise_for_status()
-
     response_results = response.json()["results"]
 
     for best_game in response_results:
@@ -22,27 +33,21 @@ def get_best_games():
 Дата: {best_game["released"]}
 Ссылка: https://rawg.io/games/{best_game["slug"]}""")
 
-        screenshots_url = f"https://api.rawg.io/api/games/{best_game['slug']}/screenshots"
-        params = {"key": api_key}
-        response = requests.get(screenshots_url, params=params)
-        response.raise_for_status()
-        screenshots = response.json()["results"]
+        screenshots = get_screenshots(api_key, best_game)
         print("Скринщоты: ")
         for screenshot in screenshots:
             print(screenshot["image"])
 
-        stores_url = f"https://api.rawg.io/api/games/{best_game['slug']}/stores"
-        params = {"key": api_key}
-        response = requests.get(stores_url, params=params)
-        response.raise_for_status()
-        stores = response.json()["results"]
+        stores = get_stores(api_key, best_game)
         print("Магазины для покупки:")
         for store in stores:
             print(store["url"])
 
 
 def main():
-    get_best_games()
+    load_dotenv()
+    api_key = os.getenv("api_key")
+    get_best_games(api_key)
 
 
 if __name__=="__main__":
